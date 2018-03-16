f7784cc33bd815529ba50c80246b5f91c21539aa
Javi Eurrutia <javier.eurrutia@softonic.com>
2018-03-16 11:07:49 +0100

    Add dock block to config file

--- config/transactional-event-publisher.php
+++ config/transactional-event-publisher.php
@@ -1,10 +1,39 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Service name that belongs the event messages produced.
+    |--------------------------------------------------------------------------
+    */
     'service' => 'service-name',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Middleware class where the event messages will be stored.
+    |--------------------------------------------------------------------------
+    */
     'middleware' => \Softonic\TransactionalEventPublisher\EventStoreMiddlewares\AmqpMiddleware::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Event Message class.
+    |--------------------------------------------------------------------------
+    */
     'message' =>  \Softonic\TransactionalEventPublisher\ValueObjects\EventMessage::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Message middleware. At this moment just an AMQP system.
+    |--------------------------------------------------------------------------
+    */
     'message_middleware' => 'amqp',
+
+    /*
+    |--------------------------------------------------------------------------
+    | AMQP properties separated by key
+    |--------------------------------------------------------------------------
+    */
     'properties' => [
         'amqp' => [
             'host'                => 'localhost',
