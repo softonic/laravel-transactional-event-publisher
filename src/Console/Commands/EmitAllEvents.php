7397fca8056937884c2dc44760f8e8258156675c
Jose Manuel Cardona <josemanuel.cardona@softonic.com>
2020-07-13 11:20:50 +0200

    Improved emit all events

--- src/Console/Commands/EmitAllEvents.php
+++ src/Console/Commands/EmitAllEvents.php
@@ -3,24 +3,19 @@
 namespace Softonic\TransactionalEventPublisher\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Database\Eloquent\Collection;
 use Softonic\TransactionalEventPublisher\Jobs\SendDomainEvents;
 use Softonic\TransactionalEventPublisher\Model\DomainEvent;
 
 class EmitAllEvents extends Command
 {
     /**
-     * Create a new command instance.
-     *
-     */
-    private const CHUNK_SIZE = 1000;
-
-    /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'event-sourcing:emit-all {queueConnection=database}';
+    protected $signature = 'event-sourcing:emit-all
+        {queueConnection=database : Queue connection to be used to send the messages}
+        {--unbufferedConnection= : Indicate the unbuffered connection (MySQL) for large amount of events}';
 
     /**
      * The console command description.
@@ -36,26 +31,23 @@ class EmitAllEvents extends Command
      */
     public function handle()
     {
-        $queueConnection       = $this->argument('queueConnection');
-        $page                  = 1;
-        $lastDomainEventToEmit = DomainEvent::limit(1)
-            ->orderBy('id', 'DESC')
-            ->first();
+        $queueConnection    = $this->argument('queueConnection');
+        $databaseConnection = $this->option('unbufferedConnection');
+        $totalEventsToEmit  = DomainEvent::count();
+        $bar                = $this->output->createProgressBar($totalEventsToEmit);
 
-        do {
-            $domainEvents = $this->getDomainEvents($page++, $lastDomainEventToEmit->id);
-            $domainEvents->each(function ($domainEvent) use ($queueConnection) {
-                SendDomainEvents::dispatch($domainEvent->message)->onConnection($queueConnection);
-            });
-        } while ($domainEvents->isNotEmpty());
-    }
+        $bar->start();
+        $bar->minSecondsBetweenRedraws(1);
 
-    public function getDomainEvents($page, $untilId): Collection
-    {
-        return DomainEvent::where('id', '<=', $untilId)
-            ->orderBy('id', 'ASC')
-            ->offset(($page - 1) * self::CHUNK_SIZE)
-            ->limit(self::CHUNK_SIZE)
-            ->get();
+        DomainEvent::on($databaseConnection)->cursor()
+            ->each(
+                function ($domainEvent) use ($queueConnection, $bar) {
+                    SendDomainEvents::dispatch($domainEvent->message)
+                        ->onConnection($queueConnection);
+                    $bar->advance();
+                }
+            );
+
+        $bar->finish();
     }
 }
