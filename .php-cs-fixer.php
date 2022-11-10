bc01c22e071004e39ca7770019ec352b1c86fa66
Sergio Novo <22319383+Serginyu@users.noreply.github.com>
2022-11-10 18:19:54 +0100

    upgrade phpcsfixer and add dockerfile

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -14,7 +14,7 @@ return $config->setRules([
         'no_empty_phpdoc' => true,
         'no_empty_comment' => true,
         'no_leading_import_slash' => true,
-        'no_trailing_comma_in_singleline_array' => true,
+        'no_trailing_comma_in_singleline' => true,
         'no_unused_imports' => true,
         'ordered_imports' => ['imports_order' => null, 'sort_algorithm' => 'alpha'],
         'phpdoc_add_missing_param_annotation' => ['only_untyped' => true],
